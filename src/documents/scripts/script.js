45d4c0de4d87d3244941f25ee0c1b4d9df9aea1a
Michael D Mooring <Mike@mdm.cc>
2013-02-04 19:19:53 +0800

    Reverting package.json

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -10,11 +10,11 @@ $(document).ready(function(){
     // inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling.
     // outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport.
     $('#intro').parallax("50%", 0, 0.1, true);
-    $('.blog').parallax("50%", 0, 0.1, true);
+    $('#blog').parallax("50%", 0, 0.1, true);
     $('.bg').parallax("5%", 3000, 0.8, true);
     $('.bg2').parallax("30%", 3500, 0.8, true);
-    $('.convo').parallax("50%", 0, 0.1, true);
-    $('.social').parallax("50%", 0, 0.1, true);
+    $('#convo').parallax("50%", 0, 0.1, true);
+    $('#social').parallax("50%", 0, 0.1, true);
 })
     $('#navbar').scrollspy();
 
