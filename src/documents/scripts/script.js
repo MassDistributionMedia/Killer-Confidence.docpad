602cd87657173157cba6a1091f35d36860a95e7d
Michael D Mooring <Mike@mdm.cc>
2013-02-04 22:52:20 +0800

    Onward CSS !

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -20,8 +20,19 @@ $(document).ready(function(){
 
 $(".collapse").collapse()
 
+$(window).scroll(function () {       
+
+        if ($(this).scrollTop() > 649) {
+        $('#upTop').css('opacity','1');
+    } else{
+        $('#upTop').css('opacity','0');
+    }
+
+});
+
 // page fade effect - this simple trick first waits for page to load(images, content) and after it's loaded fades it. You can remove it by deleting this section.
 // read documentation
 $(document).ready(function() {
     $('#page').fadeTo(1500, 1);
 });
+
